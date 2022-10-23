64e70f3b0bb9bf6610643894aad4ab6565a12c80
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2022-10-23 18:23:14 +0000

    Sun, Oct 23, 2022, 6:23 PM +00:00

--- calculator/calculator.c
+++ calculator/calculator.c
@@ -4,9 +4,14 @@
 int main(void)
 
 {
+    //promt user for value of x
     int x = get_int("x: ");
+    //promt user for value of y
     int y = get_int("y: ");
+    //educational purposes, creation of z as an alterantive to printing x+y without saving the value
+    int z = x+y;
+    //print option 1: printing z which saves the result of x+y
+    printf("%i\n",z);
+    //print option 2: printing just x+y without saving the value
     printf("%i\n",x+y);
-
-
 }
