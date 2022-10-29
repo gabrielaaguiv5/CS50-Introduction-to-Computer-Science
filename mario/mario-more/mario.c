1eee888cef618128d7e14d329446829d204b937f
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2022-10-29 22:39:00 +0000

    Sat, Oct 29, 2022, 10:39 PM +00:00

--- mario/mario-more/mario.c
+++ mario/mario-more/mario.c
@@ -1,7 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
+    //promt user for input for piramid
+    //get_int cause it should only be numbers
+    do{
+
+    }
+    while
     
 }
