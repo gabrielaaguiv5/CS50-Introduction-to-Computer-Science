9711689d32a953903ecacf513fa051df0ca70b50
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2022-10-29 22:56:42 +0000

    Sat, Oct 29, 2022, 10:56 PM +00:00

--- mario/mario.c
+++ mario/mario.c
@@ -3,12 +3,16 @@
 
 int main(void)
 {
+    int level;
     //promt user for input for piramid
     //get_int cause it should only be numbers
     do{
         level = get_int("Height: ");
     }
-    while (level > 8);
+    while (level > 8 || level < 0);
+
+
     //print a number of spaces and hashes for each row
+    for  
 
 }
