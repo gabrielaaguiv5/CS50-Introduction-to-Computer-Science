0766527751785dfb0dfa2e305d0e5779424becdd
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2022-10-31 01:26:54 +0000

    Mon, Oct 31, 2022, 1:26 AM +00:00

--- mario/mario.c
+++ mario/mario.c
@@ -12,7 +12,7 @@ int main(void)
     while (level > 8 || level < 0);
 
     //print a number of spaces and hashes for each row
-    for (int rows = 0; rows < level; rows++){
+    for (int rows = 1; rows <= level; rows++){
         for (int spaces = 0; spaces < level){
 
         do{
