1c0cc916b8d68a9beb2bc72544cb248d50ca7c3d
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2022-10-31 18:33:06 +0000

    Mon, Oct 31, 2022, 6:33 PM +00:00

--- cash/cash.c
+++ cash/cash.c
@@ -1,21 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int cents;
-int calculate_quarters(cents);
-int calculate_dimes(cents);
-int calculate_nickels(cents);
-int calculate_pennies(cents);
+int get_cents(void);
+int calculate_quarters(int cents);
+int calculate_dimes(int cents);
+int calculate_nickels(int cents);
+int calculate_pennies(int cents);
 
 int main(void)
 {
     // Ask how many cents the customer is owed //I implement the question "Change owed: "
-    do
-    {
-        cents = get_int("Change owed: ");
-    }
-    while (cents < 0);
 
+        int cents = get_cents();
 
     // Calculate the number of quarters to give the customer
     int quarters = calculate_quarters(cents);
