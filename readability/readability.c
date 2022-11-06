918a215d9faa1b2eff930385fdce4b9fbe80c2da
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2022-11-06 21:39:04 +0000

    Sun, Nov 6, 2022, 9:39 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,7 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
+    //Prompt user for text to test
+    string text = get_string("Text: ");
 
+    //Count number of letters
+    //Count number of words
+    //Count number of sentences
+
+    
 }
