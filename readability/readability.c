9a3cc8eebcb13bbfbe0e8676bc2ce178dd2bf266
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2022-11-06 21:48:24 +0000

    Sun, Nov 6, 2022, 9:48 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,16 +1,25 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int number_letters(string text);
+int number_words(string text);
+int number_sentences(string text);
+
 int main(void)
 {
     //Prompt user for text to test
     string text = get_string("Text: ");
 
     //Count number of letters
+    int letters = number_letters(text);
     //Count number of words
+    int words = number_words(text);
     //Count number of sentences
+    int sentences = number_sentences(text);
 
     //Apply Coleman-Liau formula
+    int grade = (0.0588*letters/words*100)-(0.296*sentences)-15.8;
 
     //Print Grade X
+    printf("Grade %i\n", grade);
 }
