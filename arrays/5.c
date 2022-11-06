958e90858d3bf570ce0e3053c7bc339373e248df
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2022-11-06 04:12:54 +0000

    Sun, Nov 6, 2022, 4:12 AM +00:00

--- arrays/5.c
+++ arrays/5.c
@@ -5,9 +5,10 @@ int main(void)
 {
     int n = get_int("How many scores? ");
     int scores[n];
+    int i = 0;
     float na;
 
-    for (int i = 0; i < n; i++)
+    for (i < n; i++)
     {
         scores[i] = get_int("Score: ");
 
