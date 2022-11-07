33d936d47c5a21d58b7d3663eaa7b25b57f25b00
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2022-11-07 01:28:24 +0000

    Mon, Nov 7, 2022, 1:28 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -6,5 +6,10 @@ int main(int argc, string argv[])
     //Promt user for an integer to be called k
     //If not given a single integer for k printf Usage: ./caesar key return 1 as value of main
 
+    //There are 26 characters in the alphabet but if k is 27 that should signify B which is A+1
+
+    //When k is the wanted k printf plaintext: with get_string
+
+    //With given plaintext printf ciphertext: which is plaintext rotated by k positions and return 0 as main
 
 }
