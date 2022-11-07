58532596d24c66d95e0c7595c46453677ca0ec7a
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2022-11-07 21:51:29 +0000

    Mon, Nov 7, 2022, 9:51 PM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -80,6 +80,13 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    
-    return;
+for (int i = 0; i < voter_count; i++)
+    {
+        if (strcmp(candidates[i].name, name) == 0)
+        {
+            candidates[i].votes = candidates[i].votes + 1;
+            return true;
+        }
+    }
+    return false;
 }
