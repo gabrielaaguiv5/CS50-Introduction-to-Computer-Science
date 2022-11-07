0a637a45bf42009ecb4dfe5b0cc88081680448ec
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2022-11-07 05:44:14 +0000

    Mon, Nov 7, 2022, 5:44 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -4,6 +4,7 @@
 #include <ctype.h>
 #include <stdlib.h>
 
+bool only_digits(string argv[]);
 char rotate(char c, int n);
 
 int main(int argc, string argv[])
@@ -11,7 +12,7 @@ int main(int argc, string argv[])
     string key = argv[1];
     //If not given a single integer for k printf Usage: ./caesar key return 1 as value of main
 
-    if (argc != 2 || argc > 2)
+    if (argc != 2 || argc > 2 || !only_digits(argv[1]))
     {
         printf("Usage: ./caesar key\n");
         return 1;
@@ -19,18 +20,6 @@ int main(int argc, string argv[])
 
     //Promt user for an integer to be called k
     //Check if given k is digits
-    bool only_digits(string argv[]);
-    {
-        for (int i = 0, n = strlen(argv[1]); i < n; i++)
-        {
-            if (isdigit(argv[1][i]))
-            {
-                return true;
-            }
-        }
-        printf("Usage: ./caesar key\n");
-        return false;
-     }
 
     //The given key 'k'
     //There are 26 characters in the alphabet but if k is 27 that should signify B which is A+1
@@ -47,6 +36,23 @@ int main(int argc, string argv[])
 
 return 0;
 }
+
+bool only_digits(string argv[]);
+    {
+        for (int i = 0, n = strlen(argv[1]); i < n; i++)
+        {
+            if (isdigit(argv[1][i]))
+            {
+                return true;
+            }
+        }
+        return false;
+     }
+
+
+
+
+
 
 
 char rotate(char c, int n)
