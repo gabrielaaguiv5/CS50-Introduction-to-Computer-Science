2912b0c915fbafd3c29a0b315af62c816dec8cd0
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2022-11-07 06:40:24 +0000

    Mon, Nov 7, 2022, 6:40 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -19,6 +19,7 @@ int main(int argc, string argv[])
     }
     int k = atoi(argv[1]);
 
+    
 
 
     //Promt user for an integer to be called k
@@ -28,7 +29,7 @@ int main(int argc, string argv[])
     //There are 26 characters in the alphabet but if k is 27 that should signify B which is A+1
 
     //When k is the wanted k printf plaintext: with get_string
-    string plaintext = get_string("plaintext: ");
+
     //With given plaintext printf ciphertext: which is plaintext rotated by k positions and return 0 as main
     printf("ciphertext: ");
     for (int i = 0, nm = strlen(plaintext); i < nm; i++)
