bffea6b789a3d7267739ff2433be8c91986e2575
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2022-11-07 05:06:34 +0000

    Mon, Nov 7, 2022, 5:06 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -4,9 +4,19 @@
 #include <ctype.h>
 #include <stdlib.h>
 
+char rotate(char *, int n);
+
 int main(int argc, string argv[])
 {
     string key = argv[1];
+    //If not given a single integer for k printf Usage: ./caesar key return 1 as value of main
+
+    if (argc != 2 || argc > 2)
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+
     //Promt user for an integer to be called k
     //Check if given k is digits
     bool only_digits(string argv[]);
@@ -18,24 +28,32 @@ int main(int argc, string argv[])
                 return true;
             }
         }
-        return false;
-    }
-
-    //If not given a single integer for k printf Usage: ./caesar key return 1 as value of main
-    if (argc != 2 || argc > 2)
-    {
         printf("Usage: ./caesar key\n");
-        return 1;
-    }
+        return false;
+     }
 
     //The given key 'k'
     //There are 26 characters in the alphabet but if k is 27 that should signify B which is A+1
     int k = atoi(argv[1]);
     //When k is the wanted k printf plaintext: with get_string
-    string plaintext = get_string("plaintext: ");
-
-
+    char *plaintext = get_string("plaintext: ");
     //With given plaintext printf ciphertext: which is plaintext rotated by k positions and return 0 as main
+    printf("ciphertext: ");
+    rotate(plaintext, k);
+
+
+
+
+
+
 return 0;
 }
 
+
+char rotate(char *t, int n)
+{
+    for (int i = 0, np = strlen(plaintext); i < np; i++)
+    {
+
+    }
+}
