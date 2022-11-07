da6626acca62e713f3cbfd3ef850791f50cbcdfe
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2022-11-07 04:27:11 +0000

    Mon, Nov 7, 2022, 4:27 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -21,7 +21,7 @@ int main(int argc, string argv[])
     }
 
     //If not given a single integer for k printf Usage: ./caesar key return 1 as value of main
-    if (argc != 2 || argc > 2 || only_digits(argv))
+    if (argc != 2 || argc > 2 || only_digits(argv) =)
     {
         printf("Usage: ./caesar key\n");
         return 1;
@@ -32,6 +32,8 @@ int main(int argc, string argv[])
     int k = atoi(argv[1]);
     //When k is the wanted k printf plaintext: with get_string
     string plaintext = get_string("plaintext: ");
+
+
     //With given plaintext printf ciphertext: which is plaintext rotated by k positions and return 0 as main
 return 0;
 }
