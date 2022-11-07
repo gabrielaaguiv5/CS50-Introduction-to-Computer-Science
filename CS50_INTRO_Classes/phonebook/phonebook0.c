78405b1cbd3fe2b25fc936879f19692cd8acd2d4
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2022-11-07 17:46:09 +0000

    Mon, Nov 7, 2022, 5:46 PM +00:00

--- CS50_INTRO_Classes/phonebook/phonebook0.c
+++ CS50_INTRO_Classes/phonebook/phonebook0.c
@@ -11,7 +11,7 @@ int main(void)
     {
         if (strcmp(names[i], "David") == 0)
         {
-            printf("Found %s\n", numbers[]);
+            printf("Found %s\n", numbers[i]);
             return 0;
         }
     }
