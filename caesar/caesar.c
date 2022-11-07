ca2830a5224c4f0c58f4d14f4e0d3c94fc78137e
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2022-11-07 03:40:34 +0000

    Mon, Nov 7, 2022, 3:40 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -3,15 +3,13 @@
 #include <string.h>
 #include <ctype.h>
 
-bool only_digits(string argv[]);
-
 int main(int argc, string argv[])
 {
     //Promt user for an integer to be called k
     //If not given a single integer for k printf Usage: ./caesar key return 1 as value of main
     if (argc != 2 || argc > 2)
     {
-        printf("Usage: ./caesar key");
+        printf("Usage: ./caesar key\n");
         return 1;
     }
 
@@ -24,14 +22,3 @@ int main(int argc, string argv[])
 return 0;
 }
 
-bool only_digits(string argv[])
-{
-    for (int i = 0, n = strlen(argv[]); i < n; i++)
-    {
-        if (isdigit(argv[i]))
-        {
-            return true;
-        }
-    }
-    return false;
-}
