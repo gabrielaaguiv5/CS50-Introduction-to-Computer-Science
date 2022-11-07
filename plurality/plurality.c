94fa22e43480d50a5ef2e4bb1dfe1df728030416
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2022-11-07 22:30:53 +0000

    Mon, Nov 7, 2022, 10:30 PM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -89,5 +89,10 @@ for (int i = 0; i < candidate_count; i++)
         if (candidates[i].votes > max)
             max = candidates[i].votes;
     }
-     printf("%s\n", candidates[0].name);
+for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes == max)
+            printf("%s\n", candidates[i].name);;
+    }
+
 }
