a9884868071958672374b13ab4a33cabab8ce438
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2022-11-07 02:26:44 +0000

    Mon, Nov 7, 2022, 2:26 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -5,13 +5,15 @@ int main(int argc, string argv[])
 {
     //Promt user for an integer to be called k
     //If not given a single integer for k printf Usage: ./caesar key return 1 as value of main
-
-    //There are 26 characters in the alphabet but if k is 27 that should signify B which is A+1
-    if (argc != 2 || argc > 2 || argv[1] )
+    if (argc != 2 || argc > 2)
     {
         printf("Usage: ./caesar key");
         return 1;
     }
+
+    //The given key 'k' 
+    //There are 26 characters in the alphabet but if k is 27 that should signify B which is A+1
+
     //When k is the wanted k printf plaintext: with get_string
 
     //With given plaintext printf ciphertext: which is plaintext rotated by k positions and return 0 as main
