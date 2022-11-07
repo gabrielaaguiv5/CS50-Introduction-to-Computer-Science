b349bcddb8c2972b7f175ef755b20ae6ca4014cc
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2022-11-07 22:09:41 +0000

    Mon, Nov 7, 2022, 10:09 PM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -81,16 +81,15 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    int length = sizeof(votes)/sizeof(candidates[0].votes);
 
     int max = candidates[0].votes;
 
-for (int i = 0; i < length; i++)
+for (int i = 0; i < candidate_count; i++)
     {
         if (candidates[i].votes > max)
         {
             max = candidates[i].votes;
         }
     }
-    printf("%i\n", "candidates[i].name");
+    printf("%i\n", candidates[i].name);
 }
