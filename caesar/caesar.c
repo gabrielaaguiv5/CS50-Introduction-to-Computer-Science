5c87569a42ffc38c6c916058c51eebea3eb183cb
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2022-11-07 01:23:34 +0000

    Mon, Nov 7, 2022, 1:23 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -4,6 +4,7 @@
 int main(int argc, string argv[])
 {
     //Promt user for an integer to be called k
+    //If not given a single integer for k printf Usage: ./caesar key return 1 as value of main
 
-    
+
 }
