8b40cd06994807c2bd7a1f5e08e2378601c8c4dc
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2022-11-07 04:18:14 +0000

    Mon, Nov 7, 2022, 4:18 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -5,6 +5,8 @@
 
 int main(int argc, string argv[])
 {
+    //Promt user for an integer to be called k
+    //Check if given k is digits
     bool only_digits(string argv[]);
     {
         for (int i = 0, n = strlen(argv[1]); i < n; i++)
@@ -16,16 +18,13 @@ int main(int argc, string argv[])
         }
         return false;
     }
-    //Promt user for an integer to be called k
+
     //If not given a single integer for k printf Usage: ./caesar key return 1 as value of main
-    if (argc != 2 || argc > 2)
+    if (argc != 2 || argc > 2 || only_digits(argv))
     {
         printf("Usage: ./caesar key\n");
         return 1;
     }
-
-
-
 
     //The given key 'k'
     //There are 26 characters in the alphabet but if k is 27 that should signify B which is A+1
