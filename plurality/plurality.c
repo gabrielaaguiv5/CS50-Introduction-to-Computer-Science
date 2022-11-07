84b8847fb4eae89dfc5c101de3924e7d3da4800f
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2022-11-07 22:07:35 +0000

    Mon, Nov 7, 2022, 10:07 PM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -67,7 +67,7 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
-   for (int i = 0; i < voter_count; i++)
+   for (int i = 0; i++)
     {
         if (strcmp(candidates[i].name, name) == 0)
         {
