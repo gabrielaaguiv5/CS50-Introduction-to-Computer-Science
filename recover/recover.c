ee1e80362bb845414636e18c48af795c6c899b99
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2022-11-13 03:28:28 +0000

    Sun, Nov 13, 2022, 3:28 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -21,34 +21,41 @@ int main(int argc, char *argv[])
         return 2;
     }
 
+    //Declare buffer BYTE
     BYTE buffer[512];
 
+    //Declare integer of found images
     int image = 0;
 
+    //Create file for output of found images
     FILE *output = NULL;
 
+    //For jpg files to load in output
     char *filename = malloc(8 * sizeof(char));
 
     while(fread(buffer, sizeof(char), 512, file))
     {
+        //Checking first 4 bytes
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
+            //Free data if no found jpeg
             if(output != NULL)
             {
                 fclose(output);
             }
-
+            //Load images in output
             sprintf(filename, "%03i.jpg", image);
             output = fopen(filename, "w");
 
             image++;
         }
-
+        //Load images in output
         if (output != NULL)
         {
             fwrite(buffer, sizeof(char), 512, output);
         }
     }
+    //Free all memory
     free(filename);
     if(output != NULL)
     {
