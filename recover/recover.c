34229ce68778f95e1d74f21cbcb39c1c1fc29283
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2022-11-13 01:34:05 +0000

    Sun, Nov 13, 2022, 1:34 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+    int main(int argc, char *argv[])
+{
+    // Check command-line arguments
+    if (argc != 4)
+    {
+        printf("Usage: ./volume input.wav output.wav factor\n");
+        return 1;
+    }
+
+    // Open files and determine scaling factor
+    FILE *input = fopen(argv[1], "r");
+    if (input == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    FILE *output = fopen(argv[2], "w");
+    if (output == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+
 }
