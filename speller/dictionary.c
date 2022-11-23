fd032217b36f60c9f39873f4fb8fe34e4e1b2b4d
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2022-11-23 17:54:49 +0000

    Wed, Nov 23, 2022, 5:54 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -23,8 +23,8 @@ node;
 //For function size
 int dsize = 0;
 
-//Error found when 26 letters of the alphabet, so expanded to
-const unsigned int N = 2;
+//Error found when 26 letters of the alphabet, so expanded to size of 143.091, words in the dictionary file
+const unsigned int N = 143091;
 
 // Hash table
 //Array of node pointers, each points to a node, for each letter of the alphabet
