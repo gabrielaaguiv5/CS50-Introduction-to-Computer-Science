fb2daea50f1888843f872d84ee79d9ba9ef22bb5
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2022-11-26 04:02:10 +0000

    Sat, Nov 26, 2022, 4:02 AM +00:00

--- sentimental-hello/hello.py
+++ sentimental-hello/hello.py
@@ -1,7 +1,7 @@
 # Hello world in python
 from cs50 import get_string
 
-#Get input ans save in variable answer
+# Get input ans save in variable answer
 answer = get_string("What's your name? ")
-#Print answer
+# Print answer
 print(f"hello, {answer}")
