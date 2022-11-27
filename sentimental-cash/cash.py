b078dca7be0b71c4315db8b0e1581cfec5bfa702
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2022-11-27 18:59:31 +0000

    Sun, Nov 27, 2022, 6:59 PM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -2,33 +2,17 @@
 
 def main():
 
+    q = cents + 1
+
+    print(q)
+
+
     while True:
         cents = int(input("Change owed: "))
         if cents > 0:
             break
     return cents
 
-def quarters():
-    cents = cents
-    quarters = cents/25
-    return quarters
 
-def dimes():
-    cents = cents - (quarters * 25)
-    dimes = cents/10
-    return dimes
-
-def nickels():
-    cents = cents - (dimes * 10)
-    nickels = cents/5
-    return nickels
-
-def pennies():
-    cents = cents - (nickels * 5)
-    pennies = cents/1
-    return pennies
-
-coins = quarters + dimes + nickels + pennies
-    print(coins)
 
 main()
