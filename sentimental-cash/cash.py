bfc4e88fe998af0c57f6e724f1c5870747af3f8c
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2022-11-27 19:35:24 +0000

    Sun, Nov 27, 2022, 7:35 PM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -11,24 +11,5 @@ def main():
     coins = quarters + dimes + nickels + pennies
     print(coins)
 
-def quarters():
-    cents = cents
-    quarters = cents/25
-    return quarters
-
-def dimes():
-    cents = cents - (quarters * 25)
-    dimes = cents/10
-    return dimes
-
-def nickels():
-    cents = cents - (dimes * 10)
-    nickels = cents/5
-    return nickels
-
-def pennies():
-    cents = cents - (nickels * 5)
-    pennies = cents/1
-    return pennies
 
 main()
