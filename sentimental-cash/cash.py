0da1df60cbbdffc43514e2a308e681ad7c4ad909
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2022-11-27 19:03:57 +0000

    Sun, Nov 27, 2022, 7:03 PM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,16 +1,17 @@
-# Cash in Python
 from cs50 import get_int
 
+
 def main():
-
-    for i in range(n):
-        print("#")
-
-def get_cents():
-    while True:
-        n = get_int("Change owed: ")
-        if n > 0:
-            break
-    return n
-
-main()
+   height = get_height()
+   for i in range(height):
+ 9 print("#")
+10
+11
+12 def get_height():
+13 while True:
+14 n = get_int("Height: ")
+15 if n > 0:
+16 return n
+17
+18
+19 main()
