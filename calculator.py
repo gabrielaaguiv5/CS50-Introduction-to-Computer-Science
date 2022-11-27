522d867847f3fdf7137faa29b37d3e4d482ca0ba
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2022-11-27 16:56:37 +0000

    Sun, Nov 27, 2022, 4:56 PM +00:00

--- calculator.py
+++ calculator.py
@@ -2,8 +2,14 @@ import cs50
 try:
     x = cs50.get_int("X: ")
 
+except ValueError:
+    print("This is not an int!")
+
 try:
     y = cs50.get_int("Y: ")
+
+except ValueError:
+    print("This is not an int!")
 
 z = x + y
 
