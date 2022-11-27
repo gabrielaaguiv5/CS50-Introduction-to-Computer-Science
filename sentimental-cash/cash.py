4053cc6889996c672a09b1ac4230eb44b5409f3f
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2022-11-27 19:40:27 +0000

    Sun, Nov 27, 2022, 7:40 PM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -2,14 +2,15 @@
 
 def main():
 
-    while True:
-        cents = int(input("Change owed: "))
-        if cents > 0:
-            break
-    return cents
+    cents = get_cents()
+
 
     coins = quarters + dimes + nickels + pennies
     print(coins)
-
+def get_cents():
+    while True:
+        cents = int(input("Change owed: "))
+        if cents > 0:
+            return cents
 
 main()
