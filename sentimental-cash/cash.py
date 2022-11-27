3a0edf9a0a7f9443596cc8391256fad6a09ecb14
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2022-11-27 19:41:20 +0000

    Sun, Nov 27, 2022, 7:41 PM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -4,9 +4,6 @@ def main():
 
     cents = get_cents()
 
-
-    coins = quarters + dimes + nickels + pennies
-    print(coins)
 def get_cents():
     while True:
         cents = int(input("Change owed: "))
