a4aa7c93f59325b0907e32aa8eebe9ac81be2764
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2022-11-27 18:40:25 +0000

    Sun, Nov 27, 2022, 6:40 PM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,30 +1,35 @@
 # Cash in Python
 
-def main()
-cents = get_cents()
-quarters = calculate_quarters()
-dimes = calculate_dimes()
-nickels = calculate_nickels()
-pennies = calculate_pennies()
+def main():
+    cents = get_cents()
+    quarters = calculate_quarters()
+    dimes = calculate_dimes()
+    nickels = calculate_nickels()
+    pennies = calculate_pennies()
 
-coins = quarters + dimes + nickels + pennies
+    coins = quarters + dimes + nickels + pennies
+    print(coins)
 
 def get_cents():
     while True:
         cents = int(input("Change owed: "))
         if cents > 0:
             break
-    return cents
+        return cents
 
 def calculate_quarters():
     return cents/25
 
 def calculate_dimes():
-    
+    cents = cents - (quarters * 25)
     return cents/10
 
 def calculate_nickels():
+    cents = cents - (dimes * 10)
     return cents/5
 
 def calculate_pennies():
+    cents = cents - (nickels * 5)
     return cents/1
+
+main()
