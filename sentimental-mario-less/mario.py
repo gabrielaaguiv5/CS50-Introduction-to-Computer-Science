969211de0f1d628da92b767169b37d0f6421d996
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2022-11-27 23:53:36 +0000

    Sun, Nov 27, 2022, 11:53 PM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,14 +1,12 @@
 # Mario in Python
 
 def main():
-    c = 0
+    c = 1
     rows = 1
     level = get_height()
     for i in range(1, rows, 1):
-        c += 1
         for k in range(1, level-c, 1):
             print(" ", end="")
-            rows += 1
             print()
             for j in range(0, rows - 1, 1):
                 print("#", end="")
