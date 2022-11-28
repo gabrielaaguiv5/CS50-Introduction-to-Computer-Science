6d6b1b85448a96de8347510823c3896c0e7815a6
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2022-11-28 00:38:46 +0000

    Mon, Nov 28, 2022, 12:38 AM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,13 +1,12 @@
 # Mario in Python
 
 def main():
-    level = get_height()
+    level = get_height() # Get height input with function get_height
     for row in range(1, level+1):
         for spaces in range(level-row):
-            print(" ", end="")
-
+            print(" ", end="") # Print in row i level-row spaces
         for stars in range(row):
-            print("#", end="")
+            print("#", end="") # Print in rown i, i number of #
         print()
 
 def get_height():
@@ -16,7 +15,7 @@ def get_height():
             n = int(input("Height: "))
             if n > 0:
                 if n <= 8:
-                    break
+                    break # Among the bigger than zero filter only to accept lower than 8
         except ValueError:
             print("That's not an integer!")
     return n
