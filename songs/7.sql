8618d17786deca9b8e5a0ac03028f3cddccdf84c
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2022-12-11 03:31:05 +0000

    Sun, Dec 11, 2022, 3:31 AM +00:00

--- songs/7.sql
+++ songs/7.sql
@@ -1,2 +1,2 @@
 # Display table with only one output of average of the energy of all songs by Drake.
-SELECT AVG(energy FROM songs WHERE artist_id IN (SELECT id FROM artists WHERE name = "Drake")) FROM songs;
+SELECT AVG(energy) FROM songs WHERE artist_id IN (SELECT id FROM artists WHERE name = "Drake");
