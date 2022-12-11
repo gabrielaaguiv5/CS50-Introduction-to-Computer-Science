3c93bbfb3f9bfc192785a9888139f4307f029051
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2022-12-11 03:15:05 +0000

    Sun, Dec 11, 2022, 3:15 AM +00:00

--- songs/4.sql
+++ songs/4.sql
@@ -1,2 +1,2 @@
 # Display songs with: danceability, energy, valence MORE THAN 0.75.
-SELECT FROM WHERE AND AND AND;
+SELECT name FROM songs WHERE danceability > 0.75 AND energy > 0.75 AND valence > 0.75;
