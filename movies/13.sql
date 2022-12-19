c6dd669460a51bb8893e61e866bfd3ff8fdba967
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2022-12-19 00:01:48 +0000

    Mon, Dec 19, 2022, 12:01 AM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,1 +1,1 @@
-SELECT name FROM people JOIN stars IN stars.person_id = people.id JOIN movies IN movies.id = stars.movie_id WHERE 
+SELECT name FROM people JOIN stars ON stars.person_id = people.id JOIN movies ON movies.id = stars.movie_id WHERE movie_id IN (SELECT person_id WHERE )
