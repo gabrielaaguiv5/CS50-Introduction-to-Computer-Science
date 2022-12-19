6cf9b4a39df9a4f6aa3907a7d2c14f6706ea2a67
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2022-12-19 00:03:47 +0000

    Mon, Dec 19, 2022, 12:03 AM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,1 +1,1 @@
-SELECT name FROM people JOIN stars ON stars.person_id = people.id JOIN movies ON movies.id = stars.movie_id WHERE movie_id IN (SELECT id FROM movies WHERE name = "Kevin Bacon");
+SELECT name FROM people JOIN stars ON stars.person_id = people.id JOIN movies ON movies.id = stars.movie_id WHERE movie_id IN (SELECT id FROM movies WHERE name LIKE "Kevin Bacon");
