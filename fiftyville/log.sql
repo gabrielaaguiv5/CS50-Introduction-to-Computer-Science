4b267996d2cf0a70cfdee936feec78a97a89d3b2
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2022-12-22 01:13:35 +0000

    Thu, Dec 22, 2022, 1:13 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,4 +1,7 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
--- Let's first discover the information we can access to with the databse.
-.schema 
+-- Let's first discover the information we can access to with the databse fiftyville.db.
+.schema
+
+-- We shall begin at the crime scene, learning from witnesses testimonies and security details.
+SELECT description FROM crime_scene_reports WHERE month = 7 AND day= 
