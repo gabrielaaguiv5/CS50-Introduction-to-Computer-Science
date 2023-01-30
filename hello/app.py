0064e7c5070be0a435d1de07ec236143dba1672c
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2023-01-30 02:12:27 +0000

    Sun, Jan 29, 2023, 9:12 PM -05:00

--- hello/app.py
+++ hello/app.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     name = request.args.get("name", "world")
-    return render_template("index.html", placeholder=name)
+    return render_template("index.html")
 
