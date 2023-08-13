5c2830dd7643945798e7af1f11d86acca1fc494a
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2023-08-13 05:01:49 +0000

    Sun, Aug 13, 2023, 12:01 AM -05:00

--- project/popup.js
+++ project/popup.js
@@ -5,10 +5,17 @@ chrome.tabs.query({}, tabs => {
       const link = document.createElement('a');
 
       link.textContent = tab.title || tab.url;
-      link.href = tab.url;
-      link.target = '_blank'; // Open in a new tab
+      link.href = `javascript:redirectToTab(${tab.id});`; // Use JavaScript function
+      link.addEventListener('click', event => {
+        event.preventDefault(); // Prevent default link behavior
+      });
 
       listItem.appendChild(link);
       tabList.appendChild(listItem);
     });
   });
+
+  function redirectToTab(tabId) {
+    chrome.tabs.update(tabId, { active: true });
+    window.close(); // Close the popup after redirecting
+  }
