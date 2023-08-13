2eae3c22e8d11fd47f02d2c5b37041a916095c23
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2023-08-13 20:26:57 +0000

    Sun, Aug 13, 2023, 3:26 PM -05:00

--- project/popup.js
+++ project/popup.js
@@ -5,10 +5,10 @@ chrome.tabs.query({currentWindow: true}, tabs => // Just access to tabs on the c
   const tabList = document.getElementById('tabList'); // Access to the ID of each tab - This will become the list
   tabs.forEach(tab =>
     {
-    const listTab = document.createElement('li'); // Makes each ID into a li element for the <ul> in popup.html
-    const link = document.createElement('a'); // Makes each li element into a shortcut for the currently opened tab
+    const listTab = document.createElement('li'); // Makes each Tab into a li element for the <ul> in popup.html
+    const link = document.createElement('a'); // Makes each li element into a shortcut for the currently opened Tab
 
-    link.textContent = tab.title || tab.url; // The text on the list will be exacltly how the Tab's shown in the browser's title bar or in the page's tab
+    link.textContent = tab.title || tab.url; // The text on the list will be exacltly how the Tab's shown in the browser's title bar
     link.href = '#'; // Placeholder for the link that will be created for Tab shortcut
     link.addEventListener('click', event => // The link/shortcut replicates the clicking action
     {
@@ -16,8 +16,8 @@ chrome.tabs.query({currentWindow: true}, tabs => // Just access to tabs on the c
       activateTab(tab.id);  // The event involves for a click to replicate clicking on a Tab in the current Window, hence redirecting
     });
 
-    listTab.appendChild(link); // Link (Event Listenter) created
-    tabList.appendChild(listTab); 
+    listTab.appendChild(link);
+    tabList.appendChild(listTab);
   });
 });
 
