df0a556e1a56fad290099ef3fdcdd746d2c71c27
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2023-08-13 20:17:05 +0000

    Sun, Aug 13, 2023, 3:17 PM -05:00

--- project/popup.js
+++ project/popup.js
@@ -1,21 +1,21 @@
 // Tabs Sorter by Gabriela Aguilar 2023 - Back End
 
-chrome.tabs.query({currentWindow: true}, tabs =>
+chrome.tabs.query({currentWindow: true}, tabs => // Just access to tabs on the current Window
   {
-  const tabList = document.getElementById('tabList');
+  const tabList = document.getElementById('tabList'); // Access to the ID of each tab - This will become the list
   tabs.forEach(tab =>
     {
-    const listItem = document.createElement('li');
-    const link = document.createElement('a');
+    const listTab = document.createElement('li'); // Makes each ID into a li element for the <ul> in popup.html
+    const link = document.createElement('a'); // Makes each li element into a shortcut for the currently opened tab
 
-    link.textContent = tab.title || tab.url;
+    link.textContent = tab.title || tab.url; // Identify Tab per how it appears on the Tab 
     link.href = '#'; // Set a placeholder href
     link.addEventListener('click', event => {
       event.preventDefault(); // Prevent default link behavior
       activateTab(tab.id);
     });
 
-    listItem.appendChild(link);
+    listTab.appendChild(link);
     tabList.appendChild(listItem);
   });
 });
