c1f113536802e354c24ac31c5018b0550f489ada
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2023-08-13 03:45:23 +0000

    Sat, Aug 12, 2023, 10:45 PM -05:00

--- project/background.js
+++ project/background.js
@@ -2,3 +2,15 @@ chrome.runtime.onInstalled.addListener(() => {
     // default state goes here
     // this runs ONE TIME ONLY (unless the user reinstalls your extension)
 });
+
+// content_script.js
+const titleColor = 'red'; // Change this color to your desired display color
+
+// Function to change tab title color
+function changeTabTitleColor() {
+  document.title = `%c${document.title}`;
+  document.title = document.title.replace('%c', `%c${titleColor}`);
+}
+
+// Listen for page load event and update the tab title color
+window.addEventListener('load', changeTabTitleColor);
