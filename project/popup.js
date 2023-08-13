7715c1c064aa5744fa88d9005706a7912b590a9e
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2023-08-13 05:04:37 +0000

    Sun, Aug 13, 2023, 12:04 AM -05:00

--- project/popup.js
+++ project/popup.js
@@ -1,21 +1,22 @@
 chrome.tabs.query({}, tabs => {
-    const tabList = document.getElementById('tabList');
-    tabs.forEach(tab => {
-      const listItem = document.createElement('li');
-      const link = document.createElement('a');
+  const tabList = document.getElementById('tabList');
+  tabs.forEach(tab => {
+    const listItem = document.createElement('li');
+    const link = document.createElement('a');
 
-      link.textContent = tab.title || tab.url;
-      link.href = `javascript:redirectToTab(${tab.id});`; // Use JavaScript function
-      link.addEventListener('click', event => {
-        event.preventDefault(); // Prevent default link behavior
-      });
-
-      listItem.appendChild(link);
-      tabList.appendChild(listItem);
+    link.textContent = tab.title || tab.url;
+    link.href = '#'; // Set a placeholder href
+    link.addEventListener('click', event => {
+      event.preventDefault(); // Prevent default link behavior
+      activateTab(tab.id);
     });
-  });
 
-  function redirectToTab(tabId) {
-    chrome.tabs.update(tabId, { active: true });
-    window.close(); // Close the popup after redirecting
-  }
+    listItem.appendChild(link);
+    tabList.appendChild(listItem);
+  });
+});
+
+function activateTab(tabId) {
+  chrome.tabs.update(tabId, { active: true });
+  window.close(); // Close the popup after activating the tab
+}
