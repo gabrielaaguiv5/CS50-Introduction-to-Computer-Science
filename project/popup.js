96604a9a7ee9a0fde2e16b18bd4c0c853bb22db9
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2023-08-13 20:23:57 +0000

    Sun, Aug 13, 2023, 3:23 PM -05:00

--- project/popup.js
+++ project/popup.js
@@ -9,18 +9,19 @@ chrome.tabs.query({currentWindow: true}, tabs => // Just access to tabs on the c
     const link = document.createElement('a'); // Makes each li element into a shortcut for the currently opened tab
 
     link.textContent = tab.title || tab.url; // The text on the list will be exacltly how the Tab's shown in the browser's title bar or in the page's tab
-    link.href = '#'; // Set a placeholder href
-    link.addEventListener('click', event => {
-      event.preventDefault(); // Prevent default link behavior
-      activateTab(tab.id);
+    link.href = '#'; // Placeholder for the link that will be created for Tab shortcut
+    link.addEventListener('click', event => // The link/shortcut replicates the clicking action
+    {
+      event.preventDefault(); // To prevent for link to open brand new Tab, since the intention is to redirect
+      activateTab(tab.id);  // The event involves for a click to replicate clicking on a Tab in the current Window, hence redirecting
     });
 
     listTab.appendChild(link);
-    tabList.appendChild(listItem);
+    tabList.appendChild(listTab);
   });
 });
 
 function activateTab(tabId) {
   chrome.tabs.update(tabId, { active: true });
-  window.close(); // Close the popup after activating the tab
+  window.close(); // Close the Pop Up after activating the tab
 }
