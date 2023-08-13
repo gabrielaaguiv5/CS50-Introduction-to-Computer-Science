ae923576a76028b89425ec144d8bb5c9645f206e
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2023-08-13 04:59:14 +0000

    Sat, Aug 12, 2023, 11:59 PM -05:00

--- project/popup.js
+++ project/popup.js
@@ -3,8 +3,11 @@ chrome.tabs.query({}, tabs => {
     tabs.forEach(tab => {
       const listItem = document.createElement('li');
       const link = document.createElement('a');
-      link.href = tab.url; // Set the link's href to the tab's URL
-      link.textContent = tab.title;
+
+      link.textContent = tab.title || tab.url;
+      link.href = tab.url;
+      link.target = '_blank'; // Open in a new tab
+
       listItem.appendChild(link);
       tabList.appendChild(listItem);
     });
