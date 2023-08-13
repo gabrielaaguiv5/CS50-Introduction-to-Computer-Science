1444191b419af9053b99b7cbfc26bcbd445d37f8
gabrielaaguiv5 <114376366+gabrielaaguiv5@users.noreply.github.com>
2023-08-13 03:48:38 +0000

    Sat, Aug 12, 2023, 10:48 PM -05:00

--- project/background.js
+++ project/background.js
@@ -1,16 +1,6 @@
-chrome.runtime.onInstalled.addListener(() => {
-    // default state goes here
-    // this runs ONE TIME ONLY (unless the user reinstalls your extension)
-});
-
-// content_script.js
-const titleColor = 'red'; // Change this color to your desired display color
-
-// Function to change tab title color
-function changeTabTitleColor() {
-  document.title = `%c${document.title}`;
-  document.title = document.title.replace('%c', `%c${titleColor}`);
-}
-
-// Listen for page load event and update the tab title color
-window.addEventListener('load', changeTabTitleColor);
+// background.js
+chrome.commands.onCommand.addListener(command => {
+    if (command === "open_tab_shortcut") {
+      chrome.tabs.create({ url: "https://www.example.com" }); // Replace the URL with the desired target page
+    }
+  });
